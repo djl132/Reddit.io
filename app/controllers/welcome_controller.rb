9e3241878cc93552db5e9319b3f2f16ef456394c
DerekLin <djl132@case.edu>
2017-03-01 09:44:14 -0800

    done- questions in the previous commit

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -10,6 +10,7 @@ class WelcomeController < ApplicationController
 
 #HOW COME WHEN I TAKE THIS OUT THE TEST STILL WORKS ISN'T GET IN MY WELCOMECONTROLLERSPEC TESTING whether or not this method is reutrning anything?
 # or does the backend terminal just check for th eabailable route?
-  # def faq
+  def faq
+  end
 
 end
