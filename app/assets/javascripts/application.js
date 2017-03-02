7fbe0d6df503a782a88f720a29965c06830faab9
DerekLin <djl132@case.edu>
2017-03-01 17:17:22 -0800

    CP 14: Added Views For Index Page Using Bootstrap: error was fixed by putting require bootstrap directive to the end

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,7 @@
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
+// COMPILES AND SERVES WEB ASSETS
+
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
 // or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
@@ -12,9 +14,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+//= require turbolink
 //= require bootstrap
-
-
-// # WHAT'S THE POINT OF THIS LINE!!!!
+//= require_tree .
