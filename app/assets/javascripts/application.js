efd4bda9dbc0d88e6da548a0cc02288b98d2892f
DerekLin <djl132@case.edu>
2017-03-01 17:39:32 -0800

    CP 14: Added Views For Index Page Using Bootstrap: error was fixed by putting require bootstrap directivE BEFORE require_tree

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,13 +9,13 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file. JavaScript code in this file should be added after the last require_* statement.
 //
+//require_tree includes all files in teh directory.
+
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolink
+//= require turbolinks
 //= require bootstrap
 //= require_tree .
-
-//require_tree includes all files in teh directory.
