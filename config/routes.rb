3177740438220323ceba2b66bcc7877ae9b088d2
DerekLin <djl132@case.edu>
2017-03-04 16:39:49 -0800

    Added Posts Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
 
-  get 'welcome/about'
+  resources :posts
 
-  get 'welcome/contact'
+  get 'about' => "welcome#about"
 
   root 'welcome#index'
 end
---
+++ spec/controllers/posts_controller_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe PostsController, type: :controller do
+
+  let(:my_post){Post.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph)}
+
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+
+    it "assigns @post attribute"do
+      get :index
+
+      # check if controller's @posts was assigned my_post.
+      # by accessing instance variables using assigns
+      expect(assigns(:posts)).to eq([my_post])
+      puts assigns(:posts)
+    end
+  end
+
+  # describe "GET #show" do
+  #   it "returns http success" do
+  #     get :show
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+  #
+  # describe "GET #new" do
+  #   it "returns http success" do
+  #     get :new
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+  #
+  # describe "GET #edit" do
+  #   it "returns http success" do
+  #     get :edit
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+
+end
