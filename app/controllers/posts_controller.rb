bba4a7515c0aff4b6b28828343dbdcf9cce21d88
DerekLin <djl132@case.edu>
2017-03-04 19:26:39 -0800

    added SPAM

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,13 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
-  end
+    @filtered_posts =
+    @posts.each do |post|
+        index = @posts.find_index(post)
+        post.title = "SPAM" if index == 0 || index%5 == 0
+    end
+
+   end
 
   def show
   end
