664c8d19ed7a6ac016c1e57f8ff5809101c5f2b2
DerekLin <djl132@case.edu>
2017-03-08 15:32:46 -0800

    Added partials for _form edit and new - qeustiosn in commnt

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ApplicationRecord
-  has_many :comments, dependent: :destroy
     belongs_to :topic
+    has_many :comments, dependent: :destroy
+
+    # validation : use ACTIVERECORD (every model inherits from it)
+    validates :title, length: { minimum: 5 }, presence: true
+    validates :body, length: { minimum: 20 }, presence: true
+    validates :topic, presence: true
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,16 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+
+  # TEST MODEL'S ATTRIBUTES ARE VALID
+  # ACTIVE RECORD PROVIDES VALIDATION
+  it { is_expected.to belong_to(:topic)}
+
+  it { is_expected.to validate_presence_of(:title)}
+  it { is_expected.to validate_presence_of(:body)}
+  it { is_expected.to validate_presence_of(:topic)}
+
+  it { is_expected.to validate_length_of(:title).is_at_least(5) }
+  it { is_expected.to validate_length_of(:body).is_at_least(20) }
+
+
   let(:name) { RandomData.random_sentence }
   let(:description) { RandomData.random_paragraph }
   let(:title) { RandomData.random_sentence }
   let(:body) { RandomData.random_paragraph }
   let(:topic) {Topic.create!(name: name, description: description)}
-  
-  # THIS IS BUILT IN RIGHT?
-  let(:post) {topic.posts.create!(title:title, body: body)}
 
-  it { is_expected.to belong_to(:topic)}
+    # what is create? who does it belong ot?
+    let(:post) {topic.posts.create!(title:title, body: body)}
+
 
   describe "attributes" do
     it "has title and body attributes" do
