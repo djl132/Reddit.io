a1c77777cb72da4ee1561673e5dbef871476e900
DerekLin <djl132@case.edu>
2017-03-08 16:40:53 -0800

    DONE - quesiotns in commenst

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+
+# what is the &PROC FOR?
+
+# CREATES A FORM ELEMENT OF THE CLASS FORM-GROUP THAT USES BOOTSTRAP'S
+ # HIGHLIGHT EFFECT OF HAS-ERROR CLASS,
+  def form_group_tag(errors, &block)
+    css_class = 'form-group'
+    css_class << 'has-error' if errors.any?
+
+    # create a div of form-group class, and displays an error
+    content_tag(:div, capture(&block), class: css_class)
+  end
+
 end
