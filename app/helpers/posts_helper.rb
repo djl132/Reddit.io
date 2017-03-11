5c6254009106953aaa0cea1d595cb023b64f0589
DerekLin <djl132@case.edu>
2017-03-11 11:07:15 -0800

    Done - Added different roles

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,7 +1,7 @@
 module PostsHelper
 
 # in order to edit or delete a  post, you must be admin or the owner of the post
-  def user_is_authorized_for_posts?(post)
+  def user_is_authorized_for_post?(post)
       current_user && (current_user = post.user || current_user.admin?)
   end
 end
