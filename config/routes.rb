6a57fb1d4e1d69d7280f2da71433b0033c460532
DerekLin <djl132@case.edu>
2017-03-12 21:30:27 -0700

    however, glyphicons are still not showing up

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,12 @@ Rails.application.routes.draw do
     resources :users, only: [:new, :create]
     resources :sessions, only: [:new, :create, :destroy]
 
+# voting routes
+
+    post '/up-vote' => 'votes#up_vote', as: :up_vote
+    post '/down-vote' => 'votes#down_vote', as: :down_vote
+
+
   get 'about' => "welcome#about"
 
   root 'welcome#index'
